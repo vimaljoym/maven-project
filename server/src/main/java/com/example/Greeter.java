02a5974027221e2d9c00bcb73fea8f95a3ed3e00
vimaljoym <vimaljoym@live.com>
2019-01-05 23:53:53 +0530

    fix checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,13 @@ public class Greeter {
 
   }
 
-  //For showing the greeting message
-  public String greet(final String someone) {
+/**
+* For showing the greeting message
+* @param name
+* @return greeting string 
+*/
+ 
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
