02855d34c580b820a7685c1f42fa221bc277bf5b
vimaljoym <vimaljoym@live.com>
2019-01-06 19:25:48 +0530

    added semicolon

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,6 +18,6 @@ public class Greeter {
    * @return greeting string
    */
   public final String greet(final String someone) {
-    return String.format("Hello Mr., %s!", someone)
+    return String.format("Hello Mr., %s!", someone);
   }
 }
